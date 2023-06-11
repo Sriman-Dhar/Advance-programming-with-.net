0df9a2bd59505c5b7d5daf868504bb7696f7f8aa
Xenos <Xenos@DESKTOP-PQDIRPE>
2023-06-11 23:10:23 +0600

    Add project files.

---
+++ Xcompany/Models/TeamMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Xcompany.Models
+{
+    public class TeamMember
+    {
+        public string TeamName { get; internal set; }
+    }
+}
